3d1784c3f0e34cba114d39674bf705494267a86b
Varun Sankhyan <133485342+sankhyanvarun@users.noreply.github.com>
2025-08-04 11:55:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const fadeUpElements = document.querySelectorAll('.fade-in-up');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    fadeUpElements.forEach(element => {
+        observer.observe(element);
+    });
+
+});
